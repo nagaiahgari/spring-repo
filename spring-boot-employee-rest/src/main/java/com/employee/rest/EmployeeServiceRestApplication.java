7e9131f0c180741105aec759a0ed1499971641c0
Balaji Nagaiahgari <Balaji.nagaiahgari@gmail.com>
2021-09-22 23:54:00 -0400

    adding employee rest service project

---
+++ spring-boot-employee-rest/src/main/java/com/employee/rest/EmployeeServiceRestApplication.java
@@ -0,0 +1,13 @@
+package com.employee.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeServiceRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmployeeServiceRestApplication.class, args);
+	}
+
+}
